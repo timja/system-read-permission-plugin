a9b473e54db4c1834f3de547f971a4c81edcc6e4
Tim Jacomb <timjacomb1+github@gmail.com>
2019-08-20 07:47:02 +0100

    Use reflection to load permission from core

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # system-read-permission-plugin
+
+This is a temporary plugin which allows using the `SYSTEM_READ` permission without bumping  a plugin minimum jenkins version
+to a high baseline. Users won't be able to use the feature unless they're on newer jenkins versions though.
+
+Usage:
+```java
+Jenkins.get().checkPermission(SystemReadPermission.SYSTEM_READ);
+```
--- src/main/java/io/jenkins/plugins/systemreadpermission/SystemReadPermission.java
+++ src/main/java/io/jenkins/plugins/systemreadpermission/SystemReadPermission.java
@@ -1,14 +1,29 @@
 package io.jenkins.plugins.systemreadpermission;
 
-import hudson.Extension;
 import hudson.security.Permission;
-import hudson.security.PermissionScope;
+import hudson.util.ReflectionUtils;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jenkins.model.Jenkins;
 
 import static jenkins.model.Jenkins.ADMINISTER;
-import static jenkins.model.Jenkins.PERMISSIONS;
 
-@Extension
 public class SystemReadPermission {
 
-    public static final Permission SYSTEM_READ = new Permission(PERMISSIONS, "SystemRead", Messages._SystemReadPermission_Description(), ADMINISTER, PermissionScope.JENKINS);
+    private static final Logger LOGGER = Logger.getLogger(SystemReadPermission.class.getName());
+
+    public static Permission SYSTEM_READ;
+
+    static {
+        try {
+            SYSTEM_READ = (Permission) ReflectionUtils.getPublicProperty(Jenkins.getInstance(), "SYSTEM_READ");
+        } catch (IllegalArgumentException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            LOGGER.log(Level.FINE, "Couldn't find system read permission, falling back to ADMINISTER", e);
+            SYSTEM_READ = ADMINISTER;
+        }
+
+
+
+    }
 }
